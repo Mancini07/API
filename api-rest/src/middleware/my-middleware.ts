51f7881b5ac029b24213bfe9fe227ea1cf54cb97
Clóvis Mancini <126329429+Mancini07@users.noreply.github.com>
2025-01-28 19:38:29 -0300

    Delete api-rest directory

--- api-rest/src/middleware/my-middleware.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response, NextFunction} from "express"
-
-export function myMiddleware(request: Request, response: Response, next: NextFunction) {
-    console.log("Passou pelo middleware")
-    request.user_id = "123456"
-    return next()
-}
